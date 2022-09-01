360596b0434028c15f28fa4681b7a145fcdd47a0
ZGMauri <mauri1891.mm@gmail.com>
2022-09-01 18:09:50 -0300

    agregando fotos a los productos de la lista

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,9 +3,11 @@ import React, {useState , useEffect} from 'react'
 export default function Item({itemProp}) {
   return (
     <div>
-        <p>{itemProp.id}</p>
-        <p>{itemProp.name}</p>
-        <p>{itemProp.price}</p>
+        <b><p>Id: {itemProp.id}</p></b>
+        <b><p>Nombre: {itemProp.title}</p></b>
+        <b><p>Descripcion: {itemProp.description}</p></b>
+        <b><p>Precio: {itemProp.price}</p></b>
+        <b><p>Foto del producto: {itemProp.pictureURL}</p></b>
     </div>
   )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,5 +1,9 @@
 import React, {useState , useEffect} from 'react'
 import ItemList from './ItemList';
+import fotoCamisa from '../img/camisa.jpg';
+import fotoZapato from '../img/zapatos.jpg';
+import fotoTraje from '../img/traje.jpg';
+
 
 export default function ItemListContainer() {
   const [loading, setLoading] = useState(true);
@@ -10,9 +14,9 @@ export default function ItemListContainer() {
     let promesasProductos = new Promise((resolve, reject) => {
        setTimeout(() => {
           resolve ([
-            {id: 100, name: 'zapato nike', price: 100},
-            {id: 101, name: 'zapato adidas', price: 200},
-            {id: 102, name: 'zapato puma', price: 150},
+            {id: 100, title: 'camisa', description: 'camisa blanca formal', price: 100, pictureURL: <img src={ fotoCamisa } alt='algo'></img> },
+            {id: 101, title: 'zapato', description: 'zapatos de vestir color negro', price: 200, pictureURL: <img src={ fotoZapato } alt='algo'></img>},
+            {id: 102, title: 'saco', description: 'saco de traje con pantalon incluido', price: 150, pictureURL: <img src={ fotoTraje } alt='algo'></img>},
           ]);
        }, 2000);
     })
