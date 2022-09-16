bee6bb6b3c6cb6beca150627162bad85c9b87b51
ZGMauri <mauri1891.mm@gmail.com>
2022-09-16 03:04:40 -0300

    trabajando con context

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import GenericContext from './components/GenericContext';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        
-        <Routes>
-          <Route path="/" element={<ItemListContainer />} />
-          <Route path="/category/:idcategory" element={<ItemListContainer />} />
-          <Route path="/product/:idproduct" element={<ItemListContainer />} />
-        </Routes>
+      <GenericContext>
+        <BrowserRouter>
+          <NavBar />
+          
+          <Routes>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/category/:idcategory" element={<ItemListContainer />} />
+            <Route path="/product/:idproduct" element={<ItemListContainer />} />
+          </Routes>
 
-      </BrowserRouter>
-    </>
+        </BrowserRouter>
+      </GenericContext>
+      </>
   );
 }
 
